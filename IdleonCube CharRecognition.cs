4f9e36a5473687fb0745961e410e53e5d775eb90
ItsNuked <tito.skof@gmail.com>
2023-11-09 19:09:12 +0100

    Co-authored-by: Marcel Kozole <marcel.kozole@sestg.sc-nm.si>

---
+++ IdleonCube CharRecognition.cs
@@ -0,0 +1,19 @@
+﻿// Initialize the Tesseract engine
+   using Tesseract;
+   
+
+        using (var engine = new TesseractEngine(@"path-to-tessdata", "eng", EngineMode.Default))
+        {
+        using (var image = Pix.LoadFromFile(@"screenCapture")) //kle sm hotu d bi sm screencapture from the other part of code dubu but no worki, commentin da launcha for now
+        {
+            using (var page = engine.Process(image))
+            {
+               string recognizedText = page.GetText();
+
+              if (recognizedText.Contains("6"))
+              {
+                 // Do something when the character is recognized
+              }
+            }
+        }
+        }
