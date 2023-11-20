bd6e255efdc52b2c7bacc6e319c15b0d76923515
ItsNuked <tito.skof@gmail.com>
2023-11-20 23:17:14 +0100

    Prej delu dodou blur ne dela check tmw

--- IdelonARCADE.cs
+++ IdelonARCADE.cs
@@ -12,14 +12,17 @@ namespace Image
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args, Mat img, Mat img)
         {
-            Mat img = new Mat();        //Naredi nov material, aka save-a info of all colors of all pixels
-
-            img = CvInvoke.Imread("./img/sest.jpg");       //Reads specified image (stores in object)
-            CvInvoke.Imshow("img", img);                   //Saves image ()
+            Mat img = new Mat();                                                                    //Naredi nov material, aka save-a info of all colors of all pixels
+            img = CvInvoke.Imread("D:/C#Nuked/Cube/IdleonCubeAC/IdleonCube AC/img/sest.jpg");       //Reads specified image (stores in object)
+            CvInvoke.Imshow("img", img);                                                            //Saves image ()
             
-            CvInvoke.WaitKey();
+            CvInvoke.WaitKey();                                                                     //podobn k c++, ti pusti window odprt doklr ne prtisns karkol
+
+            Mat gaussianBlur = new Mat();
+            CvInvoke.GaussianBlur(img, gaussianBlur, new System.Drawing.Size(img.Rows, img.Cols), 7.0);
+            CvInvoke.Imshow("blurry img", gaussianBlur);  
         }
     }
 }
