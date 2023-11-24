a07eaab703109c8b60bd123ccd2edcc5f65e0a65
ItsNuked <tito.skof@gmail.com>
2023-11-24 19:13:35 +0100

    Kozole je goat
    Co-authored-by: Marcel Kozole <marcel.kozole@sestg.sc-nm.si>

--- Cube/IdelonARCADE.cs
+++ Cube/IdelonARCADE.cs
@@ -35,7 +35,23 @@ namespace Image
             CvInvoke.MinMaxLoc(result, ref minVsota, ref maxVsota, ref minLokacija, ref maxLokacija);          //Išče Minimal in Maximum vsoto ter lokacijo v "result".
             CvInvoke.Threshold(result, result, 0.85, 1, Emgu.CV.CvEnum.ThresholdType.ToZero);                                                      //Podobnost da velja kot iskani predmet, išči 85% ujemanje, 1-krat.
             
+            var matches = result.ToImage<Gray, Byte>();
 
+            for (int i = 0; i < matches.Rows; i++)
+            {
+                for (int j = 0; j < matches.Cols; j++)
+                {
+                    if (matches[i, j].Intensity > .8)
+                    {
+                        System.Drawing.Point loc = new System.Drawing.Point(i, j);
+                        System.Drawing.Rectangle box = new System.Drawing.Rectangle(loc, stevka.Size);
+
+                        CvInvoke.Rectangle(background, box, new Emgu.CV.Structure.MCvScalar(0, 255, 0), 2);
+                    }
+                }
+            }
+
+            CvInvoke.Imshow("RGBstonks", background);
             CvInvoke.Imshow("stonks", result);                                                                  //Pokaži result
             CvInvoke.WaitKey(0);                                                                                //podobn k c++, ti pusti window odprt doklr ne prtisns karkol
             CvInvoke.DestroyAllWindows();    
